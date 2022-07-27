790d239808a6b5791d5def8db75aee6fb2f31b55
Marc Kruiß <k.marc@a1.net>
2022-07-27 22:27:11 +0200

    setup typescript support

--- next-misc/pages/pets.js
+++ next-misc/pages/pets.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import Image from 'next/image' // for automatic optimization, webp convertion, lazy loading, image-loading-(blur)-placeholder
-import img from '../public/1.jpg'
-import placeholderImg from '../public/placeholder.jpg'
+import React from "react";
+import Image from "next/image"; // for automatic optimization, webp convertion, lazy loading, image-loading-(blur)-placeholder
+import img from "../public/1.jpg";
+import placeholderImg from "../public/placeholder.jpg";
 
 function PetsPage() {
   return (
     <div>
-        <Image src={img} blurDataURL={placeholderImg} alt='pet' width='280' height='420' />
-        {
-            ['1','2','3','4','5'].map(path=>{
-                return(
-                    <div key={path}>
-                        <Image src={`/${path}.jpg`} blurDataURL={placeholderImg} alt='pet' width='280' height='420' />
-                    </div>
-                )
-            })
-        }
+      <Image
+        src={img}
+        blurDataURL={placeholderImg}
+        alt="pet"
+        width="280"
+        height="420"
+      />
+      {["1", "2", "3", "4", "5"].map((path) => {
+        return (
+          <div key={path}>
+            <Image
+              src={`/${path}.jpg`}
+              blurDataURL={placeholderImg}
+              alt="pet"
+              width="280"
+              height="420"
+            />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default PetsPage
+export default PetsPage;

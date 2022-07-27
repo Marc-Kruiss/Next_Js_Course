262c5c56b9f00143799e2d65074972a7206804e3
Marc Kruiß <k.marc@a1.net>
2022-07-27 21:29:59 +0200

    Head component for SEO

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,16 +1,22 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../styles/globals.css";
-import '../styles/layout.css'
+import "../styles/layout.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-
-
-  if(Component.getLayout){ // component has own layout definition?
-    return Component.getLayout(<Component {...pageProps} />) // set custom layout
+  if (Component.getLayout) {
+    // component has own layout definition?
+    return Component.getLayout(<Component {...pageProps} />); // set custom layout
   }
-  return ( // return default layout with header and footer
+  return (
+    // return default layout with header and footer
     <>
+      <Head>
+        <title>This is a Next.js Application</title>
+        <meta name="description" content="Next.js is a great web-framework" />
+      </Head>
+
       <Header />
       <Component {...pageProps} />
       <Footer />
--- next-misc/pages/about.js
+++ next-misc/pages/about.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
+import Head from 'next/head'
 
 function About() {
-  return <h1 className="content">About</h1>;
+  return (
+    <>
+    <h1 className="content">About</h1>
+
+    </>
+  
+  );
 }
 
 export default About;

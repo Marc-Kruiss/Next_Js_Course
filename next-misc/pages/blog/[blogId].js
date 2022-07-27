bcf0a0f87305e51ac566a35ed28396ae5d87e64c
Marc Kruiß <k.marc@a1.net>
2022-07-27 23:28:47 +0200

    use of env.local file and variables (private/public)

--- next-misc/pages/blog/[blogId].js
+++ next-misc/pages/blog/[blogId].js
@@ -8,7 +8,11 @@ function Blog({ title, description }) {
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-      <h1 className="content">Article</h1>
+
+      <h1 className="content">
+        {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+        {/*{process.env.DB_USER} doesnt work because it has not perfix 'NEXT_PUBLIC' */}
+      </h1>
     </>
   );
 }
@@ -16,6 +20,12 @@ function Blog({ title, description }) {
 export default Blog;
 
 export async function getStaticPaths() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+
+  console.log(
+    `Connecting to database with username ${user} with password ${password}`
+  );
   return {
     paths: [{ params: { blogId: "1" } }],
     fallback: false,

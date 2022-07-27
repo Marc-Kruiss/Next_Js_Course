68d5f73c39daf5e78503c6242b904fa6ba2f3f34
Marc Kruiß <k.marc@a1.net>
2022-07-27 19:01:47 +0200

    use scss modules

---
+++ next-styling/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/About.module.scss";
+
+function About() {
+  return (
+    <>
+      <button className="btn btn-primary">Primary</button>
+      <div className={styles.highlightscss}>Highlight style</div>
+    </>
+  );
+}
+
+export default About;
---
+++ next-styling/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/Contact.module.scss";
+
+function Contact() {
+  return (
+    <>
+      <h2>Contact</h2>
+      <div className={styles.highlightscss}>Highlight style</div>
+    </>
+  );
+}
+
+export default Contact;

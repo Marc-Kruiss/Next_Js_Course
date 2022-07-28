72dbfe196ed8cdb44b320921962f0232b7c4024b
Marc Kruiß <k.marc@a1.net>
2022-07-28 15:59:03 +0200

    secure page when not logged in

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { getSession,useSession } from 'next-auth/react';
 
-function Blog() {
-  return (
-    <h1>Blog page</h1>
-  )
+function Blog({blogsdata}){
+    const {data:session}=useSession()
+    console.log('blog auth')
+    console.log(session)
+    return <h1>Blog Page - {blogsdata}</h1>
 }
 
 export default Blog
+
+
+export async function getServerSideProps(context){
+    const session = await getSession(context);
+    console.log(session)
+
+    if(!session){
+        return{
+            redirect:{
+                destination:'/api/auth/signin?callbackUrl=http://localhost:3000/blog',
+                permanent:false
+            }
+        }
+    }
+    return {
+        props:{
+            session,
+            blogsdata : session  ? 'List of 100 personalized blogs' : 'List of free blogs'
+        },
+    }
+}

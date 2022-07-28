502803b63bd0d073ff4d7f15262f9e7f1ae68304
Marc Kruiß <k.marc@a1.net>
2022-07-28 17:44:44 +0200

    refactor

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -4,7 +4,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 function Navbar() {
   const { data: session, status } = useSession();
-  console.log({ session, status });
+  //console.log({ session, status });
   return (
     <nav className="header">
       <h1 className="logo">

1287bad09ec49ab4e0e059b91de98fa481268841
Marc Kruiß <k.marc@a1.net>
2022-07-28 17:44:25 +0200

    next auth jwt mongodb connection

--- next-auth/pages/api/auth/[...nextauth].js
+++ next-auth/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,31 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
+  database: process.env.DB_URL,
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: "asdfghhfd",
+  },
+  callbacks: {
+    async jwt({token, user}) {
+      if (user) {
+        token.id = user.id;
+      }
+      console.log("Token/user")
+      console.log({token, user})
+      console.log("----------------")
+      return token;
+    },
+    async session({session,token}) {
+      session.user.id = token.id;
+      console.log("Session/Token")
+      console.log(session, token)
+      console.log("----------------")
+
+      return session;
+    },
+  },
 });

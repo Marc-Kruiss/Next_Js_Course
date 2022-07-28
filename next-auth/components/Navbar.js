f25b8225935b8f20a0b660cd5493efbc11554fe1
Marc Kruiß <k.marc@a1.net>
2022-07-28 14:26:05 +0200

    sign in/out by clicking

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -1,41 +1,56 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { signIn, signOut } from "next-auth/react";
 
 function Navbar() {
   return (
-    <nav className='header'>
-        <h1 className='logo'>
-            <a href="#">NextAuth</a>
-        </h1>
-        <ul className={`main-nav`}>
-            <li>
-                <Link href='/'>
-                    <a>Home</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='/dashboard'>
-                    <a>Dashboard</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='/blog'>
-                    <a>Blog</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='#'>
-                    <a>Sign In</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='#'>
-                    <a>Sign Out</a>
-                </Link>
-            </li>
-        </ul>
+    <nav className="header">
+      <h1 className="logo">
+        <a href="#">NextAuth</a>
+      </h1>
+      <ul className={`main-nav`}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/api/auth/signin">
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                signIn('github');
+              }}
+            >
+              Sign In
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/api/auth/signout">
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                signOut();
+              }}
+            >
+              Sign Out
+            </a>
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

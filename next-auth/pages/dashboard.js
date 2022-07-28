ebe288b10bf9bbe4ce3af635fb224f09cd8cf56f
Marc Kruiß <k.marc@a1.net>
2022-07-28 15:59:53 +0200

    secure page client-side

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { getSession, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 function Dashboard() {
-  return (
-    <h1>Dashboard page</h1>
-  )
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        signIn();
+      } else {
+        setLoading(false);
+      }
+    };
+    securePage();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return <h1>Dashboard page</h1>;
 }
 
-export default Dashboard
+export default Dashboard;

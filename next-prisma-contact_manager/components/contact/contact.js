3f279fbfdf0fe0390424c665a0d8b991c634de05
Marc Kruiß <k.marc@a1.net>
2022-08-01 21:06:54 +0200

    contact manager to test prisma with next.js

---
+++ next-prisma-contact_manager/components/contact/contact.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Contact({person}) {
+  console.log(person)
+  return (
+    <>
+    <li class="flex py-4 first:pt-0 last:pb-0 bg-white m-1">
+            <img class="h-10 w-10 rounded-full" src={person.avatar} alt="" />
+            <div class="ml-3 overflow-hidden">
+              <p class="text-sm font-medium text-slate-900">
+                {person.firstname}
+              </p>
+              <p class="text-sm text-slate-500 truncate">{person.email}</p>
+            </div>
+          </li>
+    </>
+          
+  );
+}
+
+export default Contact;
